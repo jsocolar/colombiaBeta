12cf65871a789629a2b5ab5aa432d851885bf46b
Jacob Socolar <jacob.socolar@gmail.com>
2020-03-03 11:27:39 -0600

    updated all workflow except hydrosheds to use sf function rather than rgdal+sp+rgeos. So far I am quite pleased, and there are clear performance improvements in places.

--- range_maps.R
+++ range_maps.R
@@ -1,41 +1,44 @@
-library(rgdal)
-library(sp)
+library(sf)
 library(magrittr)
 
 setwd('/Users/jacobsocolar/Dropbox/Work/Colombia')
 
 ##### Basic Colombia map #####
 # read in GADM colombia shapefile
-colombia <- readOGR('Data/GIS/colombia_maps/gadm36_COL_shp/gadm36_COL_0.shp')
+colombia <- st_read('Data/GIS/colombia_maps/gadm36_COL_shp/gadm36_COL_0.shp')
 
 # file consists of many disjoint polygons, representing the mainland and numerous islands. Figure out which is the mainland
 # and extract it.
-npoly <- length(colombia@polygons[[1]]@Polygons)
+npoly <- length(colombia$geometry[[1]])
 size <- rep(0,npoly)
 for(i in 1:npoly){
-  size[i] <- dim(colombia@polygons[[1]]@Polygons[[i]]@coords)[1]
+  size[i] <- dim(colombia$geometry[[1]][[i]][[1]])[1]
 }
-mainland <- colombia@polygons[[1]]@Polygons[[which(size == max(size))]] %>% 
-  list() %>% Polygons(1) %>% list() %>% SpatialPolygons()
-proj4string(mainland) <- proj4string(colombia)
-bbox(mainland)
+
+mainland <- colombia$geometry[[1]][[which(size == max(size))]] %>%
+                st_polygon() %>% st_sfc() %>% st_sf()
+st_crs(mainland) <- st_crs(colombia)
 
 # Transform to AEA conic centered on Colombia
-mainland <- spTransform(mainland, CRS("+proj=aea +lat_1=-4.2 +lat_2=12.5 +lat_0=4.1 +lon_0=-73 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"))
+mainland <- st_transform(mainland, "+proj=aea +lat_1=-4.2 +lat_2=12.5 +lat_0=4.1 +lon_0=-73 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs")
 plot(mainland)
 
 # create new polygon with 100 km buffer
-b_mainland <- rgeos::gBuffer(mainland, width = 100000)
+b_mainland <- st_buffer(mainland, dist = 100000)
 plot(b_mainland)
 
 ##### Birdlife maps #####
 botw <- "Data/GIS/birdlife_maps/BOTW/BOTW.gdb"
 fc_list = ogrListLayers(botw)
 range_maps <- sf::st_read(dsn=botw,layer="All_Species")
+birdlife.species <- unique(range_maps$SCINAME)
+nsp <- length(birdlife.species)
 
-
+map1 <- range_maps[1,]
+plot(map1)
 #dim(range_maps)
 #str(range_maps)
+
 
 ##### Hydrosheds #####
 #This code is nothing at the moment--just a place to save some bits that will potentially be useful to have in the future
