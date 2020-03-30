a0eddc70596cebf06d57ae26d6baa229be68f0a0
Jacob Socolar <jacob.socolar@gmail.com>
2020-03-29 23:14:58 -0500

    resolved very screw issues related to the fact that conda activate doesn't behave as expected when run from a bash script, see https://github.com/conda/conda/issues/7980

--- GEE_attempts.R
+++ GEE_attempts.R
@@ -4,16 +4,18 @@
 # run GEE_setup.sh from a bash shell. I make no guarantees about whether GEE_setup.sh works properly on Windows.
 # I experimented with running the entire bash script from R via system2(), but ultimately we'll have to manually input 
 # our GEE authentication keys either way, so it's quite cumbersome to do entirely via R, and we don't get the benefit of
-# complete automation anyway, since we'll need to manually input the key no matter what.
+# complete automation anyway, since we'll need to manually input the GEE key no matter what.
 
 library(reticulate)
 
 ##### Set up GEE session #####
-use_condaenv("gee_interface", conda = "auto",required = TRUE) # point reticulate to the conda environment created in GEE_setup.sh
+#Sys.setenv(PATH = paste(c("/Applications/anaconda3/bin", Sys.getenv("PATH")), collapse = .Platform$path.sep))
+
+use_condaenv('gee_interface', conda = "auto", required = TRUE) # point reticulate to the conda environment created in GEE_setup.sh
 ee <- import("ee")          # Import the Earth Engine library
 ee$Initialize()             # Trigger the authentication
-np <- import("numpy")       # Import Numpy        not sure this is necessary; it's a holdover from an example I borrowed from
-pd <- import("pandas")      # Import Pandas       ditto the above
+#np <- import("numpy")       # Import Numpy        not sure this is necessary; it's a holdover from an example I borrowed from
+#pd <- import("pandas")      # Import Pandas       ditto the above
 
 ##### Extract ALOS elevation from a given point (can be looped over our points if necessary) #####
 point_of_interest <- ee$Geometry$Point(-72.37150102, -0.631402982)
@@ -24,7 +26,7 @@ poi_elev$features[[1]]$properties$AVE_DSM
 
 ##### Extract ALOS elevations from many points #####
 # (e.g. from shapefile or dataframe); prefer solution that doesn't require staging shapefile through Google Cloud Storage
-# Write shapefile for point PSP6 as test example
+# Write shapefile for point PSP6 as test
 library(sf)
 PSP6 <- st_sf(st_sfc(st_point(c(-72.37150102, -0.631402982), "PSP6")), crs = 4326)
 st_write(PSP6, dsn = "/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/Points/PSP6/PSP6.shp", 
--- GEE_setup.sh
+++ GEE_setup.sh
@@ -1,13 +1,15 @@
 # bash script run via terminal
 # Running python 3.8.2 via conda 4.8.3, shipped via Anaconda3-2020.02-MacOSX-x86_64
+# to run, paste /Users/jacobsocolar/Dropbox/Work/Code/colombiaBeta/GEE_setup.sh to terminal
 
-conda create --name gee_interface      # Create a conda environment
+conda env remove --name gee_interface
+
+y | conda create --name gee_interface      # Create a conda environment
+
+eval "$(conda shell.bash hook)"
 
 conda activate gee_interface           # Activate the environment
 
 conda install -c conda-forge earthengine-api # Install the Earth Engine Python API
 
 earthengine authenticate          # Authenticate your access with the command line tool
-
-conda install pandas
-conda install numpy
