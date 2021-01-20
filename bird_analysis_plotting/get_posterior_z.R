06f3c20574b5ab0ddbb0a898e5f5b23f257768bc
Jacob Socolar <jacob.socolar@gmail.com>
2021-01-20 16:52:30 -0600

    added code for ppcs

--- bird_analysis_plotting/get_posterior_z.R
+++ bird_analysis_plotting/get_posterior_z.R
@@ -1,22 +1,7 @@
-v5 <- cmdstanr::read_cmdstan_csv("/Users/jacobsocolar/Dropbox/Work/Colombia/Data/Analysis/Stan_outputs/v5_first_run/occupancy_v5_threads-202012282018-1-261afe.csv")
-
-
-# draws_summary <- parsummarise_draws(v5$post_warmup_draws[1:2000,,], n_cores = 2, n_chunks = 1000)
-# print(draws_summary[draws_summary$rhat > 1.05,], n=400)
-# print(draws_summary[draws_summary$ess_bulk < 20,], n=400)
-
-draws <- posterior::as_draws_df(v5$post_warmup_draws[1:2000,,])
-# Long format: 
-bird_data <- readRDS("/Users/jacobsocolar/Dropbox/Work/Colombia/Data/Analysis/bird_stan_data4_package.RDS")
-birds <- readRDS("/Users/jacobsocolar/Dropbox/Work/Colombia/Data/Analysis/birds.RDS")
-
-z_info <- data.frame(bird_data$data[8:43])
-z_info$point <- birds$point
-z_info$species <- birds$species
-
 get_z_components <- function(draws, iter, z_info){
+  # Function to get the components of logit_psi and logit_theta that do not vary across points or visits
   b <- z_info[!duplicated(z_info$id_sp),]
-  psi_forest <- as.numeric(draws[iter, paste0("b0_sp[", b$id_sp, "]")]) + as.numeric(draws[iter, paste0("b0_fam[", b$id_fam, "]")]) +
+  logit_psi_forest <- as.numeric(draws[iter, paste0("b0_sp[", b$id_sp, "]")]) + as.numeric(draws[iter, paste0("b0_fam[", b$id_fam, "]")]) +
       as.numeric(draws[iter, "b1_lowland"])*b$lowland + 
       as.numeric(draws[iter, "b3_mountain_barrier"])*b$mountain_barrier + as.numeric(draws[iter, "b3_valley_barrier"])*b$valley_barrier +
       as.numeric(draws[iter, "b3_eastOnly"])*b$eastOnly + as.numeric(draws[iter, "b3_westOnly"])*b$westOnly + as.numeric(draws[iter, "b3_snsmOnly"])*b$snsmOnly +
@@ -31,7 +16,7 @@ get_z_components <- function(draws, iter, z_info){
       as.numeric(draws[iter, "b3_x_elevMedian_forestPresent"])*b$forestPresent*b$elevMedian +
       as.numeric(draws[iter, "b3_x_elevMedian_forestSpecialist"])*b$forestSpecialist*b$elevMedian
   
-  psi_pasture_offset <- as.numeric(draws[iter, paste0("b2_pasture_sp[", b$id_sp, "]")]) + as.numeric(draws[iter, paste0("b2_pasture_fam[", b$id_fam, "]")]) +
+  logit_psi_pasture_offset <- as.numeric(draws[iter, paste0("b2_pasture_sp[", b$id_sp, "]")]) + as.numeric(draws[iter, paste0("b2_pasture_fam[", b$id_fam, "]")]) +
       as.numeric(draws[iter, "b4_mountain_barrier"])*b$mountain_barrier + as.numeric(draws[iter, "b4_valley_barrier"])*b$valley_barrier +
       as.numeric(draws[iter, "b4_elevMedian"])*b$elevMedian + as.numeric(draws[iter, "b4_elevBreadth"])*b$elevBreadth +
       as.numeric(draws[iter, "b4_forestPresent"])*b$forestPresent + as.numeric(draws[iter, "b4_forestSpecialist"])*b$forestSpecialist +
@@ -43,38 +28,52 @@ get_z_components <- function(draws, iter, z_info){
       as.numeric(draws[iter, "b4_x_elevMedian_forestPresent"])*b$forestPresent*b$elevMedian +
       as.numeric(draws[iter, "b4_x_elevMedian_forestSpecialist"])*b$forestSpecialist*b$elevMedian
 
-  theta_forest <- as.numeric(draws[iter, paste0("d0_sp[", b$id_sp, "]")]) + as.numeric(draws[iter, paste0("d0_fam[", b$id_fam, "]")]) +
+  logit_theta_forest <- as.numeric(draws[iter, paste0("d0_sp[", b$id_sp, "]")]) + as.numeric(draws[iter, paste0("d0_fam[", b$id_fam, "]")]) +
       as.numeric(draws[iter, "d3_mass"])*b$mass + as.numeric(draws[iter, "d3_elevMedian"])*b$elevMedian +
       as.numeric(draws[iter, "d3_migratory"])*b$migratory + as.numeric(draws[iter, "d3_dietCarn"])*b$dietCarn
   
-  theta_pasture_offset <- as.numeric(draws[iter, paste0("d1_pasture_sp[", b$id_sp, "]")]) + as.numeric(draws[iter, paste0("d1_pasture_fam[", b$id_fam, "]")])
+  logit_theta_pasture_offset <- as.numeric(draws[iter, paste0("d1_pasture_sp[", b$id_sp, "]")]) + as.numeric(draws[iter, paste0("d1_pasture_fam[", b$id_fam, "]")])
   
-  return(data.frame(psi_forest = psi_forest, psi_pasture_offset = psi_pasture_offset, theta_forest = theta_forest, theta_pasture_offset = theta_pasture_offset))
+  return(data.frame(logit_psi_forest = logit_psi_forest, logit_psi_pasture_offset = logit_psi_pasture_offset, 
+                    logit_theta_forest = logit_theta_forest, logit_theta_pasture_offset = logit_theta_pasture_offset,
+                    id_sp = b$id_sp))
 }
 
-get_Z_probs <- function(draws, iter, z_info, cluster_effect = T){
+get_Z_probs <- function(draws, iter, z_info, cluster_effect = "include"){
+  # Get the posterior occupancy probabilities for each species-point.
+  # draws: a draws_df from occupancy_v5
+  # iter: a posterior iteration
+  # z_info: the $data part of a data object used in fitting, with points and species names appended from the birds dataframe
+  # cluster_effect: "include":  include fitted cluster effects
+  #                 "exclude":  exclude fitted cluster effects
+  #                 "resample": resample cluster effects from hyperparameters
+
+# output: dataframe with columns for the point, the species, the observed Q, the fitted psi, 
+#           the fitted conditional probability of at least one detection, the posterior
+#           probability that Z == 1, and the conditional detection probabilities of each visit
+  
   zc <- get_z_components(draws, iter, z_info)
   
-  if(cluster_effect){
-    logit_psi <- zc$psi_forest[z_info$id_sp] + zc$psi_pasture_offset[z_info$id_sp] * z_info$pasture +
-      as.numeric(draws[iter, paste0("b0_spCl[", format(z_info$id_spCl, scientific = F, trim = "true", justify = "none"), "]")]) +
-      as.numeric(draws[iter, paste0("b1_relev_sp[", z_info$id_sp, "]")]) * z_info$relev +
-      as.numeric(draws[iter, paste0("b1_relev2_sp[", z_info$id_sp, "]")]) * z_info$relev2 +
-      as.numeric(draws[iter, "b1_x_lowland_relev"]) * z_info$lowland * z_info$relev +
-      as.numeric(draws[iter, "b1_x_lowland_relev2"]) * z_info$lowland * z_info$relev2 +
-      as.numeric(draws[iter, paste0("b5_distance_to_range_sp[", z_info$id_sp, "]")]) * z_info$distance_to_range
-  }else{
-    logit_psi <- zc$psi_forest[z_info$id_sp] + zc$psi_pasture_offset[z_info$id_sp] * z_info$pasture +
-      as.numeric(draws[iter, paste0("b1_relev_sp[", z_info$id_sp, "]")]) * z_info$relev +
-      as.numeric(draws[iter, paste0("b1_relev2_sp[", z_info$id_sp, "]")]) * z_info$relev2 +
-      as.numeric(draws[iter, "b1_x_lowland_relev"]) * z_info$lowland * z_info$relev +
-      as.numeric(draws[iter, "b1_x_lowland_relev2"]) * z_info$lowland * z_info$relev2 +
-      as.numeric(draws[iter, paste0("b5_distance_to_range_sp[", z_info$id_sp, "]")]) * z_info$distance_to_range
+  logit_psi_no_cluster <- zc$logit_psi_forest[z_info$id_sp] + zc$logit_psi_pasture_offset[z_info$id_sp] * z_info$pasture +
+    as.numeric(draws[iter, paste0("b1_relev_sp[", z_info$id_sp, "]")]) * z_info$relev +
+    as.numeric(draws[iter, paste0("b1_relev2_sp[", z_info$id_sp, "]")]) * z_info$relev2 +
+    as.numeric(draws[iter, "b1_x_lowland_relev"]) * z_info$lowland * z_info$relev +
+    as.numeric(draws[iter, "b1_x_lowland_relev2"]) * z_info$lowland * z_info$relev2 +
+    as.numeric(draws[iter, paste0("b5_distance_to_range_sp[", z_info$id_sp, "]")]) * z_info$distance_to_range
+  
+  if(cluster_effect == "include"){
+    logit_psi <- logit_psi_no_cluster +
+      as.numeric(draws[iter, paste0("b0_spCl[", format(z_info$id_spCl, scientific = F, trim = "true", justify = "none"), "]")])
+  }else if(cluster_effect == "exclude"){
+    logit_psi <- logit_psi_no_cluster
+  }else if(cluster_effect == "resample"){
+    cluster_effects <- rnorm(max(z_info$id_spCl), 0, as.numeric(draws[iter, "sigma_b0_spCl"]))
+    logit_psi <- logit_psi_no_cluster + cluster_effects[z_info$id_spCl]
   }
   
   psi <- boot::inv.logit(logit_psi)
   
-  logit_theta_matrix <- replicate(4, zc$theta_forest[z_info$id_sp] + zc$theta_pasture_offset[z_info$id_sp] * z_info$pasture) +
+  logit_theta_matrix <- replicate(4, zc$logit_theta_forest[z_info$id_sp] + zc$logit_theta_pasture_offset[z_info$id_sp] * z_info$pasture) +
     sweep(as.matrix(z_info[,c("time.1", "time.2", "time.3", "time.4")]), MARGIN = 1, as.numeric(draws[iter, paste0("d2_time_sp[", z_info$id_sp, "]")]), `*`) +
     as.numeric(draws[iter, "d2_obsSM"]) * as.matrix(z_info[,c("obsSM.1", "obsSM.2", "obsSM.3", "obsSM.4")]) +
     as.numeric(draws[iter, "d2_obsDE"]) * as.matrix(z_info[,c("obsDE.1", "obsDE.2", "obsSM.3", "obsDE.4")]) +
@@ -88,8 +87,10 @@ get_Z_probs <- function(draws, iter, z_info, cluster_effect = T){
   z0_lik <- 1-psi
   z1_prob <- z1_lik/(z1_lik + z0_lik)
   
-  df_out <- data.frame(point = z_info$point, species = z_info$species, Q = z_info$Q)
+  df_out <- data.frame(point = z_info$point, species = z_info$species, Q = z_info$Q, psi = psi)
+  df_out$pdet <- 1 - zero_det_lik
   df_out$Z_prob <- df_out$Q + (1 - df_out$Q)*z1_prob
+  df_out$theta_matrix <- theta_matrix
   
   return(df_out)
 }
