11cc6647640e1df7316d283f7e817aa27c3ebfbb
SimonCMills <smills2@sheffield.ac.uk>
2022-05-26 09:05:23 +0100

    brought dt conversion into this script

--- bird_analysis_plotting/occupancy_rasters/generate_prediction_raster.R
+++ bird_analysis_plotting/occupancy_rasters/generate_prediction_raster.R
@@ -1,120 +1,129 @@
-# generate prediction stars object
-# most of time spent per species is opening the .grd files. Looping across these, 
-# extracting relev and tdist and then saving all species to a single rds file
-# will be much faster (as pay this overhead once, rather than for each posterior)
+# Generate prediction info data objects in varying formats (stars, data.table)
 #
-# Note: To run within 30GB RAM, there are several rm(list = ls()) calls: do not
-# run with unsaved stuff in memory!
-# Would probably be able to compress saved stars files further by concatenating
-# them, but no real advantage.. 
+# Majority of time spent per species is opening the .grd files. Looping across 
+# these, extracting relev and tdist and then saving all species to a single .rds 
+# file is much faster for downstream processing (pay overhead once, rather than 
+# for each posterior). The dt and xy lookup are mainly used downstream. 
+# 
+# Generates:
+# outputs/prediction_info.rds: stars object with tdist and relev, species as bands
+# outputs/prediction_info_dt.rds: data.table object, same info as above
+# outputs/xy_info_lookup.rds: lookup table to relate data.table back to spatial
+#   object
+# 
+# Note the rm(list = ls()) calls. Can't run this locally, though mileage may vary
 
-library(dplyr); library(stars)
-
-# clear memory 
+# housekeeping ----
 rm(list = ls())
+library(dplyr); library(stars); library(data.table)
+source("bird_analysis_plotting/occupancy_rasters/helper_functions.R")
 
-# read files (think this aggregate step is redundant here.. remove)
-raster_elev_AEA <- raster::raster("data/elev_raster/elev_raster/raster_elev_AEA.grd")
-raster_agg <- raster::aggregate(raster_elev_AEA, 10)
-raster::values(raster_agg) <- 1:(raster::ncell(raster_agg))
+# read files 
+elev_stars <- read_stars("data/elev_raster/elev_raster/raster_elev_AEA.grd") %>%
+    setNames("elevation")
 
-raster_disagg <- raster::disaggregate(raster_agg, 10)
-elev_df <- raster::as.data.frame(raster_elev_AEA, xy = T) 
-elev_df$cell_id <- 1:nrow(elev_df)
-sr_df <- raster::as.data.frame(raster::crop(raster_disagg, raster_elev_AEA), xy = T)
-all.equal(elev_df[,c("x","y")], sr_df[,c("x","y")])
-elev_df$sr_id <- sr_df$elevation
-names(elev_df)[3] <- "elevation"
-
-source("bird_analysis_plotting/get_posterior/get_posterior_z_v6.R")
 bird_data <- readRDS("data/bird_stan_data6_package.RDS")
 birds <- readRDS("data/birds.RDS")
-stan_output <- readRDS("data/occupancy_v9_202201080800_summary.rds")
-draws <- posterior::as_draws_df(stan_output$draws_200)
-
-z_info <- data.frame(bird_data$data[8:41])
-z_info$point <- birds$point
-z_info$species <- birds$species
+species_names <- unique(birds$species)
 
 relev_sd <- bird_data$means_and_sds$relev_sd
 relev_offset <- bird_data$means_and_sds$relev_offset
-elev_stars <- st_as_stars(raster_elev_AEA)
 
-tdist_list <- vector("list", 1614)
-relev_list <- vector("list", 1614)
+# loop over species ----
+pred_info_list <- vector("list", 1614)
 for(i in 1:1614){
-    print(i)
-    sp <- unique(birds$species[bird_data$data$id_sp == i])
-    sp2 <- gsub("_", " ", sp)
-    
-    if(length(sp)!= 1){stop('sp does not have length 1')}
-    
-    tdist_stars <- read_stars(paste0("data/transformed_distance/transformed_distance/", sp, ".grd")) %>%
-        setNames("tdist")
-    
-    buffer_stars <- read_stars(paste0('data/buffered/buffered/', sp2, '_buffered.grd')) %>%
-        setNames("in_range") 
-    
-    # Get elevational min/max
-    sp_lower <- unique(birds$lower[birds$species == sp])
-    sp_upper <- unique(birds$upper[birds$species == sp])
-    sp_breadth <- sp_upper - sp_lower
-    
-    out <- c(buffer_stars, elev_stars, tdist_stars) %>%
-        mutate(tdist = ifelse(!is.na(in_range), tdist, NA), 
-               elevation = ifelse(!is.na(in_range), elevation, NA), 
-               # further remove elevations outside of buffered elevation
-               elevation = ifelse(elevation > (sp_lower - sp_breadth) & 
-                                      elevation < (sp_upper + sp_breadth), 
-                                  elevation, NA),
-               relev = ((elevation - sp_lower)/sp_breadth - relev_offset)/relev_sd)
-    
-    tdist_list[[i]] <- out %>% select(tdist)
-    relev_list[[i]] <- out %>% select(relev)
+     print(i)
+     sp <- unique(birds$species[bird_data$data$id_sp == i])
+     sp2 <- gsub("_", " ", sp)
+     
+     if(length(sp)!= 1){stop('sp does not have length 1')}
+     
+     tdist_stars <- read_stars(paste0("data/transformed_distance/transformed_distance/", sp, ".grd")) %>%
+         setNames("tdist")
+     
+     buffer_stars <- read_stars(paste0('data/buffered/buffered/', sp2, '_buffered.grd')) %>%
+         setNames("in_range") 
+     
+     # Get elevational min/max
+     sp_lower <- unique(birds$lower[birds$species == sp])
+     sp_upper <- unique(birds$upper[birds$species == sp])
+     sp_breadth <- sp_upper - sp_lower
+     
+     pred_info_list[[i]]  <- c(buffer_stars, elev_stars, tdist_stars) %>%
+         mutate(tdist = ifelse(!is.na(in_range), tdist, NA), 
+                elevation = ifelse(!is.na(in_range), elevation, NA), 
+                # further remove elevations outside of buffered elevation
+                elevation = ifelse(elevation > (sp_lower - sp_breadth) & 
+                                       elevation < (sp_upper + sp_breadth), 
+                                   elevation, NA),
+                relev = ((elevation - sp_lower)/sp_breadth - relev_offset)/relev_sd) %>%
+         select(tdist, relev)
 }
 
+# save list obj ----
+saveRDS(pred_info_list, "outputs/pred_info_list.rds")
+
 # manage memory
-saveRDS(tdist_list, "outputs/tdist_list.rds")
-saveRDS(relev_list, "outputs/relev_list.rds")
-rm(list = ls())
+rm(bird_data, birds); gc()
 
-# species ids
-species_names <- unique(readRDS("data/birds.RDS")$species)
+# convert to stars ----
+# note: this bit is the memory bottleneck 
+pred_info <- do.call(c, c(pred_info_list, list(along="species"))) %>%
+    st_set_dimensions("species", values = species_names)
 
-# convert to stars object
-# note: this bit is more memory hungry- RAM bumps up to ~30GB mark, hence all the 
-# garbage collection. The lapply line is to avoid the error "variable names are 
-# limited to 10000 bytes" at the merge stage. Not sure what this is about, but 
-# this fixes it.  
-tdist_list <- readRDS("outputs/tdist_list.rds")
-tdist_stars <- tdist_list %>%
-    lapply(., function(x) setNames(x, nm = paste0(sample(0:9, 3, T), collapse=""))) %>%
-    do.call(c, .) %>%
-    merge(.) %>%
-    setNames("tdist") %>%
-    st_set_dimensions("attributes", values = species_names) %>%
-    st_set_dimensions(names = c("x", "y", "species"))
+pred_info <- trim_stars(pred_info)
 
-# save stars object & manage memory
-saveRDS(tdist_stars, "outputs/tdist_stars.rds")
-rm(tdist_stars, tdist_list)
-relev_list <- readRDS("outputs/relev_list.rds")
+# save
+saveRDS(pred_info, "outputs/prediction_info.rds")
 
-# convert to stars object
-relev_stars <- relev_list %>%
-    lapply(., function(x) setNames(x, nm = paste0(sample(0:9, 3, T), collapse=""))) %>%
-    do.call(c, .) %>%
-    merge(.) %>%
-    setNames("relev") %>%
-    st_set_dimensions("attributes", values = species_names) %>%
-    st_set_dimensions(names = c("x", "y", "species"))
+# convert to dt object ----
+tdist <- pred_info %>% select("tdist")
 
-# save stars object & manage memory
-saveRDS(relev_stars, "outputs/relev_stars.rds")
-rm(relev_list)
+# extract indexing in the stars object (note: can calculate col and row indexing
+# but do this to check funs)
+col_index_stars <- tdist %>%
+    slice(1, along="species") %>%
+    mutate(id_cell = 1:n(), id_x=NA, id_y = NA) %>%
+    select(id_cell, id_x, id_y)
 
-# concatenate to single stars object
-tdist_stars <- readRDS("outputs/tdist_stars.rds")
-saveRDS(c(tdist_stars, relev_stars), "outputs/prediction_info.rds")
+xy_dim <- dim(col_index_stars)
+col_index_stars[["id_x"]] <- matrix(rep(1:xy_dim[2], each=xy_dim[1]), 
+                                    nrow=xy_dim[2], ncol=xy_dim[1])
+col_index_stars[["id_y"]] <- matrix(rep(1:xy_dim[1], xy_dim[2]), 
+                                    nrow=xy_dim[2], ncol=xy_dim[1])
 
-rm(list=ls())
+col_index_sf <- st_as_sf(col_index_stars, as_points = TRUE)
+col_index_dt <- as.data.table(col_index_sf)
+rm(col_index_stars, col_index_sf)
+
+# convert tdist to dt object
+tdist_sf <- st_as_sf(tdist, as_points = T, na.rm=F)
+tdist_dt <- as.data.table(tdist_sf)
+tdist_dt[, geometry := NULL]
+tdist_dt[,id_cell := col_index_dt$id_cell]
+tdist_dt <- tdist_dt[rowSums(!is.na(tdist_dt[,-"id_cell"])) > 0, ]
+tdist_dt <- melt(tdist_dt, id.vars="id_cell", variable.name = "species", 
+                 variable.factor=T, value.name = "tdist")
+tdist_dt <- tdist_dt[!is.na(tdist),]
+
+# manage memory
+rm(tdist, tdist_sf)
+
+## repeat for relev ----
+relev <- pred_info %>% select("relev")
+relev_sf <- st_as_sf(relev, as_points = T, na.rm=F)
+relev_dt <- as.data.table(relev_sf)
+relev_dt[, geometry := NULL]
+relev_dt[,id_cell := col_index_dt$id_cell]
+relev_dt <- relev_dt[rowSums(!is.na(relev_dt[,-"id_cell"])) > 0, ]
+relev_dt <- melt(relev_dt, id.vars="id_cell", variable.name = "species", 
+                 variable.factor=T, value.name = "relev")
+relev_dt <- relev_dt[!is.na(relev),]
+
+## merge ----
+pred_dt <- merge(tdist_dt, relev_dt, all=T, by=c("id_cell", "species"))
+pred_dt[complete.cases(pred_dt),]
+
+## save ----
+saveRDS(pred_dt, "outputs/prediction_info_dt.rds")
+saveRDS(col_index_dt, "outputs/xy_info_lookup.rds")
