bccd22eace53afce776bbf6eb05ceb1e8dbd1901
SimonCMills <smills2@sheffield.ac.uk>
2023-02-10 13:33:23 +0000

    redirect inputs/outputs

--- code/bird_lists_maps_traits/combined_bird_maps.R
+++ code/bird_lists_maps_traits/combined_bird_maps.R
@@ -2,13 +2,14 @@
 
 ###### Script dependencies: species_lists.R, ayerbe_maps.R, bird_import_and_cleaning.R, hydrosheds_extraction.R #####
 
-library(sf)
-library(ggplot2)
+# housekeeping ----
+library(sf); library(ggplot2)
 `%ni%` <- Negate(`%in%`)
-AEAstring <- "+proj=aea +lat_1=-4.2 +lat_2=12.5 +lat_0=4.1 +lon_0=-73 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
+AEAstring <- "+proj=aea +lat_1=-4.2 +lat_2=12.5 +lat_0=4.1 +lon_0=-73 +x_0=0 
+    +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs"
 
 ##### Get colombia mainland shapefile #####
-colombia <- st_read('/Users/JacobSocolar/Dropbox/Work/Colombia/Data/GIS/colombia_maps/gadm36_COL_shp/gadm36_COL_0.shp')
+colombia <- st_read('inputs/gadm36_COL_shp/gadm36_COL_0.shp')
 # file consists of many disjoint polygons, representing the mainland and numerous islands. Figure out which is the mainland and extract it.
 npoly <- length(colombia$geometry[[1]])
 size <- rep(0,npoly)
@@ -26,11 +27,12 @@ m2 <- st_simplify(mainland, dTolerance = 10000)
 
 
 ##### Buffered Ayerbe maps #####
-initial_species_list <- read.csv("/Users/jacobsocolar/Dropbox/Work/Colombia/Data/Birds/species_list_creation/initial_species_list.csv")
-ayerbe_maps <- readRDS('/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/ayerbe_maps/ayerbe_maps.RDS') # from ayerbe_maps.R
+initial_species_list <- read.csv("inputs/initial_species_list.csv")
+ayerbe_maps <- readRDS('outputs/ayerbe_maps/ayerbe_maps.RDS') # from ayerbe_maps.R
 
 ayerbe_maps$Species[ayerbe_maps$Species %ni% initial_species_list$HBW]
-# Pseudastur occidentalis; occurrence in Colombia is sketchy, and in any case this is not relevant to our points
+# Pseudastur occidentalis; occurrence in Colombia is sketchy, and in any case 
+# this is not relevant to our points
 # Trogon caligatus; updated lump
 # Corapipo altera; updated lump
 # Chlorostilbon melanorhynchus; updated lump
@@ -38,89 +40,112 @@ ayerbe_maps$Species[ayerbe_maps$Species %ni% initial_species_list$HBW]
 # Turdus assimilis; updated lump
 initial_species_list$HBW[initial_species_list$HBW %ni% ayerbe_maps$Species]
 
-ayerbe_list <- list()
-for(i in 1:nrow(initial_species_list)){
-  ayerbe_list[[i]] <- st_make_valid(ayerbe_maps[ayerbe_maps$Species == initial_species_list$HBW[i], ])
+ayerbe_list_fname <- 'outputs/ayerbe_list.RDS'
+if(!exists(ayerbe_list_fname)) {
+    
+    
+    ayerbe_list <- list()
+    for(i in 1:nrow(initial_species_list)){
+        ayerbe_list[[i]] <- st_make_valid(ayerbe_maps[ayerbe_maps$Species == initial_species_list$HBW[i], ])
+    }
+    names(ayerbe_list) <- initial_species_list$HBW
+    ayerbe_list[["Trogon violaceus"]] <- st_union(ayerbe_list[["Trogon violaceus"]], 
+                                                  ayerbe_maps[ayerbe_maps$Species == "Trogon caligatus", ])[,1]
+    ayerbe_list[["Corapipo leucorrhoa"]] <- st_union(ayerbe_list[["Corapipo leucorrhoa"]], 
+                                                     ayerbe_maps[ayerbe_maps$Species == "Corapipo altera", ])[,1]
+    ayerbe_list[["Chlorostilbon mellisugus"]] <- st_union(ayerbe_list[["Chlorostilbon mellisugus"]], 
+                                                          ayerbe_maps[ayerbe_maps$Species == "Chlorostilbon melanorhynchus", ])[,1]
+    ayerbe_list[["Basileuterus tristriatus"]] <- st_union(ayerbe_list[["Basileuterus tristriatus"]], 
+                                                          ayerbe_maps[ayerbe_maps$Species == "Basileuterus tacarcunae", ])[,1]
+    ayerbe_list[["Turdus albicollis"]] <- st_union(ayerbe_list[["Turdus albicollis"]], 
+                                                   ayerbe_maps[ayerbe_maps$Species == "Turdus assimilis", ])[,1]
+    
+    
+    ayerbe_list[["Vireo olivaceus"]] <- mainland
+    T_ochr <- st_transform(st_read("inputs/ayerbe_updates/ayerbe_missing2/Troglodytes_ochraceus/Troglodytes_ochraceus.kml"), 
+                           AEAstring)
+    ayerbe_list[["Troglodytes ochraceus"]] <- T_ochr
+    
+    ##### Deal with Grallaria rufula #####
+    
+    rufula_lato <- ayerbe_list[["Grallaria rufula"]]
+    rufula_ig <- st_transform(st_read("inputs/rufula_updates/Iguaque.kml"), AEAstring)
+    rufula_west <- st_transform(st_read("inputs/rufula_updates/westAndes.kml"), AEAstring)
+    rufula_central <- st_transform(st_read("inputs/rufula_updates/centralSouthernAndes.kml"), AEAstring)
+    rufula_east <- st_transform(st_read("inputs/rufula_updates/eastAndes.kml"), AEAstring)
+    rufula_snsm <- st_transform(st_read("inputs/rufula_updates/snsm.kml"), AEAstring)
+    
+    G_alvarezi <- st_difference(rufula_lato, st_union(rufula_central, st_union(rufula_east, rufula_snsm)))
+    G_spatiator <- st_difference(rufula_lato, st_union(rufula_west, st_union(rufula_central, rufula_east)))
+    G_rufula <- st_difference(rufula_lato, st_union(rufula_ig, st_union(rufula_west, st_union(rufula_central, rufula_snsm))))
+    G_saturata <- st_union(rufula_ig, st_difference(rufula_lato, st_union(rufula_west, st_union(rufula_east, rufula_snsm))))
+    
+    ayerbe_list[["Grallaria rufula"]] <- G_rufula
+    ayerbe_list[["Grallaria spatiator"]] <- G_spatiator
+    ayerbe_list[["Grallaria alvarezi"]] <- G_alvarezi
+    ayerbe_list[["Grallaria saturata"]] <- G_saturata
+    
+    saveRDS(ayerbe_list, file = ayerbe_list_fname)
+} else {
+    ayerbe_list <- readRDS(ayerbe_list_fname)   
 }
-names(ayerbe_list) <- initial_species_list$HBW
-ayerbe_list[["Trogon violaceus"]] <- st_union(ayerbe_list[["Trogon violaceus"]], ayerbe_maps[ayerbe_maps$Species == "Trogon caligatus", ])[,1]
-ayerbe_list[["Corapipo leucorrhoa"]] <- st_union(ayerbe_list[["Corapipo leucorrhoa"]], ayerbe_maps[ayerbe_maps$Species == "Corapipo altera", ])[,1]
-ayerbe_list[["Chlorostilbon mellisugus"]] <- st_union(ayerbe_list[["Chlorostilbon mellisugus"]], ayerbe_maps[ayerbe_maps$Species == "Chlorostilbon melanorhynchus", ])[,1]
-ayerbe_list[["Basileuterus tristriatus"]] <- st_union(ayerbe_list[["Basileuterus tristriatus"]], ayerbe_maps[ayerbe_maps$Species == "Basileuterus tacarcunae", ])[,1]
-ayerbe_list[["Turdus albicollis"]] <- st_union(ayerbe_list[["Turdus albicollis"]], ayerbe_maps[ayerbe_maps$Species == "Turdus assimilis", ])[,1]
-
-
-ayerbe_list[["Vireo olivaceus"]] <- mainland
-T_ochr <- st_transform(st_read("/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/ayerbe_updates/ayerbe_missing2/Troglodytes_ochraceus/Troglodytes_ochraceus.kml"), AEAstring)
-ayerbe_list[["Troglodytes ochraceus"]] <- T_ochr
-
-##### Deal with Grallaria rufula #####
-
-rufula_lato <- ayerbe_list[["Grallaria rufula"]]
-rufula_ig <- st_transform(st_read("/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/rufula_updates/Iguaque.kml"), AEAstring)
-rufula_west <- st_transform(st_read("/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/rufula_updates/westAndes.kml"), AEAstring)
-rufula_central <- st_transform(st_read("/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/rufula_updates/centralSouthernAndes.kml"), AEAstring)
-rufula_east <- st_transform(st_read("/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/rufula_updates/eastAndes.kml"), AEAstring)
-rufula_snsm <- st_transform(st_read("/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/rufula_updates/snsm.kml"), AEAstring)
-
-G_alvarezi <- st_difference(rufula_lato, st_union(rufula_central, st_union(rufula_east, rufula_snsm)))
-G_spatiator <- st_difference(rufula_lato, st_union(rufula_west, st_union(rufula_central, rufula_east)))
-G_rufula <- st_difference(rufula_lato, st_union(rufula_ig, st_union(rufula_west, st_union(rufula_central, rufula_snsm))))
-G_saturata <- st_union(rufula_ig, st_difference(rufula_lato, st_union(rufula_west, st_union(rufula_east, rufula_snsm))))
-
-ayerbe_list[["Grallaria rufula"]] <- G_rufula
-ayerbe_list[["Grallaria spatiator"]] <- G_spatiator
-ayerbe_list[["Grallaria alvarezi"]] <- G_alvarezi
-ayerbe_list[["Grallaria saturata"]] <- G_saturata
-
-saveRDS(ayerbe_list, file = '/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/ayerbe_maps/ayerbe_list.RDS')
-ayerbe_list <- readRDS('/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/ayerbe_maps/ayerbe_list.RDS')
 
 ##### Buffered ayerbe maps #####
-source('/Users/jacobsocolar/Dropbox/Work/Code/colombiaBeta/GIS_processing/hydrosheds_extraction.R')
-
-clipping_polygons <- list(amazon_orinoco = st_transform(amazon_orinoco, AEAstring), magdalena_east = st_transform(magdalena_east, AEAstring),
-                          magdalena_west = st_transform(magdalena_west, AEAstring), cauca_east = st_transform(cauca_east, AEAstring),
-                          cauca_west = st_transform(cauca_west, AEAstring), pasto = st_transform(pasto, AEAstring),
-                          pacific = st_transform(pacific, AEAstring), snsm = st_transform(snsm, AEAstring),
-                          guajira_perija = st_transform(guajira_perija, AEAstring), catatumbo = st_transform(catatumbo, AEAstring),
-                          tacarcuna = st_transform(tacarcuna, AEAstring))
-
-
-ayerbe_buffered_ranges <- list()
-for(i in 1:nrow(initial_species_list)){
-  print(i)
-  species <- initial_species_list$HBW[i]
-  sp_range <- ayerbe_list[[species]]
-  if(nrow(sp_range) == 0){stop()}
-  buffer_list <- list()
-  counter <- 0
-  for(j in 1:length(clipping_polygons)){
-    cp <- clipping_polygons[[j]]
-    clipped_range <- st_intersection(cp, sp_range)
-    if(nrow(clipped_range) > 0){
-      if(nrow(clipped_range) != 1){clipped_range <- st_union(clipped_range)}   # this is an issue for Ayerbe splits/HBW lumps, e.g. Frederickena
-      counter <- counter + 1
-      if(i != 1045){cr <- clipped_range[, 1]}else{cr <- clipped_range}
-      buffered_cr <- st_buffer(cr, 160000)
-      buffer_list[[counter]] <- st_intersection(buffered_cr, cp)
+ayerbe_fname <- "outputs/ayerbe_buffered_ranges.RDS"
+if(!exists(ayerbe_fname)) {
+    source('code/GIS_processing/hydrosheds_extraction.R')
+    
+    clipping_polygons <- list(amazon_orinoco = st_transform(amazon_orinoco, AEAstring), 
+                              magdalena_east = st_transform(magdalena_east, AEAstring),
+                              magdalena_west = st_transform(magdalena_west, AEAstring), 
+                              cauca_east = st_transform(cauca_east, AEAstring),
+                              cauca_west = st_transform(cauca_west, AEAstring), 
+                              pasto = st_transform(pasto, AEAstring),
+                              pacific = st_transform(pacific, AEAstring), 
+                              snsm = st_transform(snsm, AEAstring),
+                              guajira_perija = st_transform(guajira_perija, AEAstring), 
+                              catatumbo = st_transform(catatumbo, AEAstring),
+                              tacarcuna = st_transform(tacarcuna, AEAstring))
+    
+    
+    ayerbe_buffered_ranges <- list()
+    for(i in 1:nrow(initial_species_list)){
+        print(i)
+        species <- initial_species_list$HBW[i]
+        sp_range <- ayerbe_list[[species]]
+        if(nrow(sp_range) == 0){stop()}
+        buffer_list <- list()
+        counter <- 0
+        for(j in 1:length(clipping_polygons)){
+            cp <- clipping_polygons[[j]]
+            clipped_range <- st_intersection(cp, sp_range)
+            if(nrow(clipped_range) > 0){
+                # this is an issue for Ayerbe splits/HBW lumps, e.g. Frederickena
+                if(nrow(clipped_range) != 1){clipped_range <- st_union(clipped_range)}  
+                counter <- counter + 1
+                if(i != 1045){cr <- clipped_range[, 1]}else{cr <- clipped_range}
+                buffered_cr <- st_buffer(cr, 160000)
+                buffer_list[[counter]] <- st_intersection(buffered_cr, cp)
+            }
+        }
+        buffered_output <- st_union(buffer_list[[1]])
+        if(length(buffer_list) > 1){
+            for(k in 2:length(buffer_list)){
+                buffered_output <- st_union(buffered_output, buffer_list[[k]])
+            }
+        }
+        ayerbe_buffered_ranges[[i]] <- buffered_output
+        names(ayerbe_buffered_ranges)[i] <- species
     }
-  }
-  buffered_output <- st_union(buffer_list[[1]])
-  if(length(buffer_list) > 1){
-    for(k in 2:length(buffer_list)){
-      buffered_output <- st_union(buffered_output, buffer_list[[k]])
-    }
-  }
-  ayerbe_buffered_ranges[[i]] <- buffered_output
-  names(ayerbe_buffered_ranges)[i] <- species
+    
+    saveRDS(ayerbe_buffered_ranges, file = ayerbe_fname)
+} else {
+    ayerbe_buffered_ranges <- readRDS(ayerbe_fname)
 }
 
-saveRDS(ayerbe_buffered_ranges, file = "/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/ayerbe_maps/ayerbe_buffered_ranges.RDS")
-ayerbe_buffered_ranges <- readRDS("/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/ayerbe_maps/ayerbe_buffered_ranges.RDS")
 
-bird_surveys <- readRDS('/Users/jacobsocolar/Dropbox/Work/Colombia/Data/Analysis/bird_surveys_current.RDS')
-colombia_points <- st_as_sf(readRDS("/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/Points/all_pts.RDS"), 
+bird_surveys <- readRDS('inputs/bird_surveys_current.RDS')
+colombia_points <- st_as_sf(readRDS("inputs/all_pts.RDS"), 
                                                         coords = c('lon', 'lat'), crs = 4326)
 bird_points <- st_transform(colombia_points[colombia_points$point %in% bird_surveys$point_names, ], AEAstring)
 
@@ -172,54 +197,58 @@ for(i in 1:length(bad_species)){
 
 
 ##### Combined map errors #####
-ayerbe_list <- readRDS("/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/ayerbe_maps/ayerbe_list.RDS")
-
-update_files <- list.files('/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/ayerbe_map_updates', recursive = T, full.names = T)
-update_species <- gsub("_", " ", list.files('/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/ayerbe_map_updates'))
-
-for(i in 1:length(update_files)){
-  update <- st_transform(st_zm(st_read(update_files[i])), AEAstring)
-  ayerbe_list[[update_species[i]]] <- st_union(ayerbe_list[[update_species[i]]], update)
+ayerbe_upd_list_fname <- 'outputs/ayerbe_list_updated.RDS'
+if(!exists(ayerbe_upd_list_fname)) {
+    update_files <- list.files('inputs/ayerbe_map_updates', recursive = T, full.names = T)
+    update_species <- gsub("_", " ", list.files('inputs/ayerbe_map_updates'))
+    
+    for(i in 1:length(update_files)){
+        update <- st_transform(st_zm(st_read(update_files[i])), AEAstring)
+        ayerbe_list[[update_species[i]]] <- st_union(ayerbe_list[[update_species[i]]], update)
+    }
+    
+    ayerbe_list_updated <- ayerbe_list
+    saveRDS(ayerbe_list_updated, file = ayerbe_upd_list_fname)
+} else {
+    ayerbe_list_updated <- readRDS(ayerbe_upd_list_fname)  
 }
-
-ayerbe_list_updated <- ayerbe_list
-
-
-saveRDS(ayerbe_list_updated, file = '/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/ayerbe_maps/ayerbe_list_updated.RDS')
-ayerbe_list_updated <- readRDS('/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/ayerbe_maps/ayerbe_list_updated.RDS')
 
 # Redo the buffering for the updated ranges
-ayerbe_buffered_ranges_updated <- ayerbe_buffered_ranges
-for(i in which(names(ayerbe_buffered_ranges) %in% update_species)){
-  print(i)
-  us <- names(ayerbe_buffered_ranges)[i]
-  print(us)
-  range <- ayerbe_list_updated[[us]]
-  buffer_list <- list()
-  counter <- 0
-  for(j in 1:length(clipping_polygons)){
-    cp <- clipping_polygons[[j]]
-    clipped_range <- st_intersection(cp, range)
-    if(nrow(clipped_range) > 0){
-      if(nrow(clipped_range) != 1){clipped_range <- st_union(clipped_range)}   # this is an issue for Ayerbe splits/HBW lumps, e.g. Frederickena
-      counter <- counter + 1
-      cr <- clipped_range[, 1]
-      buffered_cr <- st_buffer(cr, 160000)
-      buffer_list[[counter]] <- st_intersection(buffered_cr, cp)
+update_buff_ranges_fname <- "outputs/ayerbe_buffered_ranges_updated.RDS"
+if(!exists(update_buff_ranges_fname)) {
+    ayerbe_buffered_ranges_updated <- ayerbe_buffered_ranges
+    for(i in which(names(ayerbe_buffered_ranges) %in% update_species)){
+        print(i)
+        us <- names(ayerbe_buffered_ranges)[i]
+        print(us)
+        range <- ayerbe_list_updated[[us]]
+        buffer_list <- list()
+        counter <- 0
+        for(j in 1:length(clipping_polygons)){
+            cp <- clipping_polygons[[j]]
+            clipped_range <- st_intersection(cp, range)
+            if(nrow(clipped_range) > 0){
+                # this is an issue for Ayerbe splits/HBW lumps, e.g. Frederickena
+                if(nrow(clipped_range) != 1){clipped_range <- st_union(clipped_range)}
+                counter <- counter + 1
+                cr <- clipped_range[, 1]
+                buffered_cr <- st_buffer(cr, 160000)
+                buffer_list[[counter]] <- st_intersection(buffered_cr, cp)
+            }
+        }
+        buffered_output <- st_union(buffer_list[[1]])
+        if(length(buffer_list) > 1){
+            for(k in 2:length(buffer_list)){
+                buffered_output <- st_union(buffered_output, buffer_list[[k]])
+            }
+        }
+        ayerbe_buffered_ranges_updated[[i]] <- buffered_output
     }
-  }
-  buffered_output <- st_union(buffer_list[[1]])
-  if(length(buffer_list) > 1){
-    for(k in 2:length(buffer_list)){
-      buffered_output <- st_union(buffered_output, buffer_list[[k]])
-    }
-  }
-  ayerbe_buffered_ranges_updated[[i]] <- buffered_output
+    
+saveRDS(ayerbe_buffered_ranges_updated, file = update_buff_ranges_fname)
+} else {
+    ayerbe_buffered_ranges_updated <- readRDS(update_buff_ranges_fname)
 }
-
-saveRDS(ayerbe_buffered_ranges_updated, file = "/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/ayerbe_maps/ayerbe_buffered_ranges_updated.RDS")
-ayerbe_buffered_ranges_updated <- readRDS("/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/ayerbe_maps/ayerbe_buffered_ranges_updated.RDS")
-
 
 distances <- list()
 dsp <- vector()
@@ -246,17 +275,17 @@ bad_species <- dsp[mdist > d]
 
 bad_species
 
-point_distances <- as.data.frame(matrix(data = NA, nrow = nrow(initial_species_list), ncol = nrow(bird_points)))
+point_distances <- as.data.frame(matrix(data = NA, 
+                                        nrow = nrow(initial_species_list), 
+                                        ncol = nrow(bird_points)))
 names(point_distances) <- bird_points$point
 row.names(point_distances) <- initial_species_list$HBW
 
-# This is just for checking whether anything is out-of-range at 160km scale, not for actually assigning distance covariates
+# This is just for checking whether anything is out-of-range at 160km scale, 
+# not for actually assigning distance covariates
 for(i in 1:length(ayerbe_buffered_ranges_updated)){
   print(i)
   point_distances[i, ] <- st_distance(bird_points, ayerbe_buffered_ranges_updated[[i]])
 }
 
-saveRDS(point_distances, file = "/Users/jacobsocolar/Dropbox/Work/Colombia/Data/GIS/point_distances/point_distances_biogeographic_clip_ayerbe.RDS")
-
-
-
+saveRDS(point_distances, file = "outputs/point_distances_biogeographic_clip_ayerbe.RDS")
