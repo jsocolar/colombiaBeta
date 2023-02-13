2d0a4b2fcfc13fbbd03a80ffddc85c08fd535d6a
SimonCMills <smills2@sheffield.ac.uk>
2023-02-13 11:53:57 +0000

    minor edits

--- code/GIS_processing/get_mainland.R
+++ code/GIS_processing/get_mainland.R
@@ -1,13 +1,10 @@
 # get shapefile for mainland Colombia
 
-# housekeeping ----
-library(sf)
-
 # read in GADM colombia shapefile
-colombia <- st_read('inputs/GIS/colombia_maps/gadm36_COL_shp/gadm36_COL_0.shp')
+colombia <- st_read('inputs/gadm36_COL_shp/gadm36_COL_0.shp')
 
-# File consists of many disjoint polygons, representing the mainland and numerous islands. Figure out which is the mainland
-# and extract it.
+# File consists of many disjoint polygons, representing the mainland and numerous 
+# islands. Figure out which is the mainland and extract it.
 npoly <- length(colombia$geometry[[1]])
 size <- rep(0,npoly)
 for(i in 1:npoly){
@@ -17,5 +14,3 @@ for(i in 1:npoly){
 mainland <- colombia$geometry[[1]][[which(size == max(size))]] %>%
   st_polygon() %>% st_sfc() %>% st_sf()
 st_crs(mainland) <- st_crs(colombia)
-
-# no outputs, where does this get called from?
